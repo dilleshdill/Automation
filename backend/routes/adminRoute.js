4ccc3f5f71ce59b192d6322d8185a0f9fccb3181
dilleshdill <dilleshnakkina222@gmail.com>
2026-01-06 21:52:49 +0530

    ..

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,13 +1,11 @@
 import express from "express"
-import { adminLogin, registerAdmin } from "../controllers/adminRegisterController.js"
+import { adminLogin, registerAdmin,getAuctions } from "../controllers/adminRegisterController.js"
 import { protectAdmin } from "../middleware/protectAdmin.js"
-import { isAdmin } from "../middleware/isAdmin.js"
 
 const adminRoute = express.Router()
 
-// routes
-
 adminRoute.post('/register',registerAdmin)
 adminRoute.post('/login',adminLogin)
+
 
 export default adminRoute;
--- backend/routes/auctionRoute.js
+++ backend/routes/auctionRoute.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import { protectAdmin } from '../middleware/protectAdmin.js';
-import { addPlayersAndFranchises, createAuction } from '../controllers/auctionController.js';
+import { addPlayersAndFranchises, createAuction,getAllAuctions } from '../controllers/auctionController.js';
 const auctionRoute = express.Router()
-
-// routes
 
 auctionRoute.post('/create-auction',protectAdmin,createAuction)
 auctionRoute.post('/develop-auction',protectAdmin,addPlayersAndFranchises)
+auctionRoute.get('/get-auction-list',protectAdmin,getAllAuctions)
 
 export default auctionRoute;

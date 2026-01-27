95902ef8c7f430088fb8e8672c2f3fd16c82f26b
haritarun <tarunbommana798@gmail.com>
2026-01-27 10:08:45 +0530

    ...

--- frontend/src/Socket/socket.js
+++ frontend/src/Socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 const DOMAIN = import.meta.env.VITE_DOMAIN
-export const socket = io(, {
+export const socket = io(DOMAIN, {
   withCredentials: true
 }
 ,{ autoConnect: true });
